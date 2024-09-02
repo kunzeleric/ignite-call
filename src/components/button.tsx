89e1cb8a81c85bc03edab50d18933445290562b9
Eric Kunzel <kunzel.eric@gmail.com>
2024-09-02 10:25:08 -0300

    refactor: included button variant prop

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,12 +3,25 @@ import { ComponentProps, ReactNode } from 'react'
 interface ButtonProps extends ComponentProps<'button'> {
   children: ReactNode
   className?: string
+  variation?: 'primary' | 'secondary'
 }
 
-export function Button({ children, className, ...props }: ButtonProps) {
+export function Button({
+  variation = 'primary',
+  children,
+  className,
+  ...props
+}: ButtonProps) {
+  const externalClasses = `${className} 
+  ${
+    variation === 'primary'
+      ? 'bg-green-500 hover:bg-green-600'
+      : 'bg-gray-200 hover:bg-gray-400'
+  }`
+
   return (
     <button
-      className={`flex text-sm justify-center items-center gap-2 px-4 py-2 rounded-md ${className} text-white duration-300 bg-green-500 hover:bg-green-600`}
+      className={`flex text-sm justify-center items-center gap-2 px-4 py-2 rounded-md ${externalClasses} text-white duration-300`}
       {...props}
     >
       {children}
