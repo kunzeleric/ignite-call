bf17971c59dd2d7caba7f3bbf9f1504ff5d3c8c2
Eric Kunzel <kunzel.eric@gmail.com>
2024-09-19 09:19:18 -0300

    feat: finished calendar days display logic

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,7 +1,17 @@
 import { getWeekDays } from '@/utils/get-week-days'
 import { CaretLeft, CaretRight } from '@phosphor-icons/react'
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import dayjs from 'dayjs'
+
+interface CalendarWeek {
+  week: number
+  days: Array<{
+    date: dayjs.Dayjs
+    disabled: boolean
+  }>
+}
+
+type CalendarWeeks = CalendarWeek[]
 
 export function Calendar() {
   const [currentDate, setCurrentDate] = useState(() => {
@@ -24,6 +34,80 @@ export function Calendar() {
 
   const currentMonth = currentDate.format('MMMM')
   const currentYear = currentDate.format('YYYY')
+
+  const calendarWeeks = useMemo(() => {
+    // creates an array of arrays representing each day in the current month
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, index) => {
+      return currentDate.set('date', index + 1)
+    })
+
+    // gets the first day of the current month
+    const firstWeekDay = currentDate.get('day')
+
+    // creates an array to fill the first week with previous month's days
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    })
+      .map((_, index) => {
+        return currentDate.subtract(index + 1, 'day')
+      })
+      .reverse()
+
+    const lastDayInCurrentMonth = currentDate.set(
+      'date',
+      currentDate.daysInMonth(),
+    )
+    const lastWeekDay = lastDayInCurrentMonth.get('day')
+
+    const nextMonthFillArray = Array.from({
+      length: 7 - (lastWeekDay + 1),
+    }).map((_, index) => {
+      return lastDayInCurrentMonth.add(index + 1, 'day')
+    })
+
+    const calendarDays = [
+      ...previousMonthFillArray.map((date) => {
+        return {
+          date,
+          disabled: true,
+        }
+      }),
+      ...daysInMonthArray.map((date) => {
+        return {
+          date,
+          disabled: false,
+        }
+      }),
+      ...nextMonthFillArray.map((date) => {
+        return {
+          date,
+          disabled: true,
+        }
+      }),
+    ]
+
+    const calendarWeeks = calendarDays.reduce<CalendarWeeks>(
+      (weeks, _, index, original) => {
+        const isNewWeek = index % 7 === 0
+
+        if (isNewWeek) {
+          weeks.push({
+            week: index / 7 + 1,
+            days: original.slice(index, index + 7),
+          })
+        }
+
+        return weeks
+      },
+      [],
+    )
+
+    return calendarWeeks
+  }, [currentDate])
+
+  console.log(calendarWeeks)
 
   return (
     <div className="">
@@ -59,30 +143,24 @@ export function Calendar() {
           </tr>
         </thead>
         <tbody>
-          <tr className="flex justify-around mt-2">
-            <td className="w-16 aspect-square"></td>
-            <td className="w-16 aspect-square"></td>
-            <td className="w-16 aspect-square"></td>
-            <td className="w-16 aspect-square"></td>
-            <td>
-              <button
-                disabled
-                className="w-16 aspect-square disabled:bg-transparent disabled:cursor-default disabled:opacity-40 bg-gray-600 [&:not(:disabled):hover]:bg-gray-500 text-center rounded-md cursor-pointer"
-              >
-                1
-              </button>
-            </td>
-            <td>
-              <button className="w-16 aspect-square disabled:bg-transparent disabled:cursor-default disabled:opacity-40 bg-gray-600 [&:not(:disabled):hover]:bg-gray-500 text-center rounded-md cursor-pointer">
-                2
-              </button>
-            </td>
-            <td>
-              <button className="w-16 aspect-square disabled:bg-transparent disabled:cursor-default disabled:opacity-40 bg-gray-600 [&:not(:disabled):hover]:bg-gray-500 text-center rounded-md cursor-pointer">
-                3
-              </button>
-            </td>
-          </tr>
+          {calendarWeeks.map(({ week, days }) => {
+            return (
+              <tr className="flex justify-around mt-2" key={week}>
+                {days.map(({ date, disabled }) => {
+                  return (
+                    <td key={date.toString()}>
+                      <button
+                        disabled={disabled}
+                        className="w-16 aspect-square disabled:bg-transparent disabled:cursor-default disabled:opacity-40 bg-gray-600 [&:not(:disabled):hover]:bg-gray-500 text-center rounded-md cursor-pointer"
+                      >
+                        {date.get('date')}
+                      </button>
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
