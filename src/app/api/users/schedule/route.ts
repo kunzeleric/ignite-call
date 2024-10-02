a9fc96a4ddc16f37f493c7f0521d00305ee2dbe8
Eric Kunzel <kunzel.eric@gmail.com>
2024-10-02 15:50:55 -0300

    feat: installed googleapi and created function to refresh token

--- src/app/api/users/schedule/route.ts
+++ src/app/api/users/schedule/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/app/lib/prisma'
 import { z } from 'zod'
 import dayjs from 'dayjs'
+import { google } from 'googleapis'
+import { getGoogleOAuthToken } from '@/app/lib/google'
 
 export async function POST(req: NextRequest) {
   const { searchParams } = new URL(req.url)
@@ -50,13 +52,42 @@ export async function POST(req: NextRequest) {
       { status: 400 },
     )
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       date: schedulingDate.toDate(),
       name,
       email,
       observations,
       user_id: user.id,
+    },
+  })
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observations,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, 'hour').format(),
+      },
+      attendees: [{ email, displayName: name }],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
